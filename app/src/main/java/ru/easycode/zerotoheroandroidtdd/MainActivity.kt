5758712e52ba4ae6cc4ffd28d2c914d0c6386f96
Огнев Иван Владимирович <ognev.iv>
2023-12-29 23:02:48 +0300

    progress

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,30 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var button: Button
+    private lateinit var textView: TextView
+    private lateinit var progressBar: ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.actionButton)
+        textView = findViewById(R.id.titleTextView)
+        progressBar = findViewById(R.id.progressBar)
+        button.setOnClickListener {
+            button.isEnabled = false
+            progressBar.visibility = View.VISIBLE
+            button.postDelayed({
+                textView.visibility = View.VISIBLE
+                progressBar.visibility = View.GONE
+                button.isEnabled = true
+            }, 1234)
+        }
     }
 }
